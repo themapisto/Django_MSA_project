ccd482114042ad302ab7c3800d718c4da31df8f0
themapisto <themapisto@naver.com>
2022-06-25 21:42:12 +0900

    API for api/products/ views.py & serializers.py & urls.py

--- products/views.py
+++ products/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from products.models import Product
+from products.serializers import ProductSerializer
 
-# Create your views here.
+
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request): # /api/products
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    def create(self, request): # /api/products
+        pass
+
+    def retrieve(self, request, pk=None): # /api/products/<str:id>
+        pass
+
+    def update(self, request, pk=None): # /api/products/<str:id>
+        pass
+
+    def destroy(self, request, pk=None): # /api/products/<str:id>
+        pass
