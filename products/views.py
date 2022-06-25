0e1ab6053844d07f5fa6fa725555d583b3e538c4
themapisto <themapisto@naver.com>
2022-06-25 22:15:11 +0900

    API for api/products/
    method create /api/products
    method retrieve , update , destroy

--- products/views.py
+++ products/views.py
@@ -18,10 +18,19 @@ class ProductViewSet(viewsets.ViewSet):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def retrieve(self, request, pk=None): # /api/products/<str:id>
-        pass
+        product= Product.objects.get(id=pk)
+        serializers = ProductSerializer(product)
+        return Response(serializers.data)
+
 
     def update(self, request, pk=None): # /api/products/<str:id>
-        pass
+        product = Product.objects.get(id=pk)
+        serializer = ProductSerializer(instance=product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
 
     def destroy(self, request, pk=None): # /api/products/<str:id>
-        pass
+        product= Product.objects.get(id=pk)
+        product.delete()
+        return Response(status=status.HTTP_202_ACCEPTED)
