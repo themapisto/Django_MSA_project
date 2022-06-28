2389c3df96075d4df1ab3271fbe71a6d7a1022b2
MZC01-KOOK <themapisto@naver.com>
2022-06-28 17:28:12 +0900

    Rabbitmq test

--- consumer.py
+++ consumer.py
@@ -8,9 +8,11 @@ channel = connection.channel()
 
 channel.queue_declare(queue='admin')
 
-def callback(ch,method,properties,body):
+
+def callback(ch, method, properties, body):
     print('Receive in admin')
     print(body)
+
 
 channel.basic_consume(queue='admin', on_message_callback=callback)
 
