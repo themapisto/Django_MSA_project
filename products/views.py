96bdc21776d8b63aa348c627efc7d3f42303bf9b
MZC01-KOOK <themapisto@naver.com>
2022-06-28 11:01:00 +0900

    user APIView insert

--- products/views.py
+++ products/views.py
@@ -1,9 +1,10 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from products.models import Product
+from products.models import Product, User
 from products.serializers import ProductSerializer
-
+import random
 
 class ProductViewSet(viewsets.ViewSet):
     def list(self, request): # /api/products
@@ -22,7 +23,6 @@ class ProductViewSet(viewsets.ViewSet):
         serializer = ProductSerializer(product)
         return Response(serializer.data)
 
-
     def update(self, request, pk=None):
         product = Product.objects.get(id=pk)
         serializer = ProductSerializer(instance=product, data=request.data)
@@ -34,3 +34,11 @@ class ProductViewSet(viewsets.ViewSet):
         product= Product.objects.get(id=pk)
         product.delete()
         return Response(status=status.HTTP_202_ACCEPTED)
+
+class UserAPIView(APIView):
+    def get(self, _):
+        users = User.objects.all()
+        user = random.choice(users)
+        return Response({
+            'id': user.id
+        })
