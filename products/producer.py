3f205922e5339a1dd1cf4b9dbf01e0d0d271f4a4
MZC01-KOOK <themapisto@naver.com>
2022-06-29 16:13:56 +0900

    producer.py consumer.py updated/deleted

--- products/producer.py
+++ products/producer.py
@@ -1,4 +1,6 @@
 # amqps://wjdihkuz:13LRz2uHVsbK80oPLnLmPfwwowPFbdBs@cougar.rmq.cloudamqp.com/wjdihkuz
+import json
+
 import pika
 
 params = pika.URLParameters('amqps://wjdihkuz:13LRz2uHVsbK80oPLnLmPfwwowPFbdBs@cougar.rmq.cloudamqp.com/wjdihkuz')
@@ -7,5 +9,6 @@ connection = pika.BlockingConnection(params)
 
 channel = connection.channel()
 
-def publish():
-    channel.basic_publish(exchange='', routing_key='main', body='hello')
+def publish(method, body):
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(exchange='', routing_key='main', body=json.dumps(body), properties=properties)
